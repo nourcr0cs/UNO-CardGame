407c173d1b63d70d0649dcedd60e778945df0b9b
nourcr0cs <nourimene2005@gmail.com>
2024-12-15 18:50:59 +0100

    .

---
+++ Card.java
@@ -0,0 +1,28 @@
+public class Card {
+
+        private String value;
+        private String color;
+        private String type;
+    
+        public Card(String value,String color,String type ) {
+            this.value = value;
+            this.color = color;
+            this.type = type;
+        }
+    
+        public String getValue() {
+            return value;
+        }
+    
+        public String getColor() {
+            return color;
+        }
+
+        public String getType() {
+            return type;
+        }
+    
+    }
+    
+    
+
---
+++ Deck.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Deck{
+
+    // attr
+    private List<Card> deck;
+    String[] colors = {"Yellow", "Red", "Blue", "Green"};
+
+    //constructor
+    public Deck() {
+        this.deck = new ArrayList<>();
+        initDeck();
+    }
+
+    // in Total we have 96 cards 
+    // number 0 : one for each color 
+    // number 1-9 : two for each color 
+    // wild cards : 4/4
+    // special cards : 3 for each color
+
+
+    public void initDeck() {
+    // First: Colored Cards From 0 to 9
+    for (int i = 0; i <= 9; i++) {
+    this.deck.add(new Card(Integer.toString(i), "Yellow","simple"));
+    this.deck.add(new Card(Integer.toString(i), "Red","simple"));
+    this.deck.add(new Card(Integer.toString(i), "Blue", "simple"));
+    this.deck.add(new Card(Integer.toString(i), "Green", "simple"));
+    }
+
+
+    for (int i = 1; i <= 9; i++) {
+    this.deck.add(new Card(Integer.toString(i), "Yellow","simple"));
+    this.deck.add(new Card(Integer.toString(i), "Red","simple"));
+    this.deck.add(new Card(Integer.toString(i), "Blue", "simple"));
+    this.deck.add(new Card(Integer.toString(i), "Green", "simple"));
+}
+
+    // 4 cards for +4 et switch color
+    for (int i = 0; i < 4; i++) {
+    this.deck.add(new Card("multi", "none","Wild"));
+    this.deck.add(new Card("+4", "none","Wild"));
+}
+
+
+    // Skip, Reverse, +2
+    for (int i = 0; i < 4; i++) {
+      String color;
+      color = colors[i];
+      this.deck.add( new Card("Skip", color, "special") );
+      this.deck.add( new Card("Reverse", color, "special") );
+      this.deck.add( new Card("+2", color, "special") );
+    }
+
+  }
+
+}
